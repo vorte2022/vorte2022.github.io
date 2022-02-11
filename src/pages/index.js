9f170d2ee0bef92ad5679c8a1b68e0bec267f51b
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-02-11 17:11:54 +0100

    First information about VORTE

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,91 @@ export default function Home() {
     <Layout>
       <section>
         <div>
-          <h1>
-            11th International Workshop on Vocabularies, Ontologies and Rules for the Enterprise
-          </h1>
-          <p className="subtitle">
-            In conjunction with the{" "}
-            <a href="https://caise22.ugent.be/">
-              34th International Conference on Advanced Information Systems
-              Engineering (CAiSE 2022)
-            </a>
-            , Leuven, Belgium, 06-10 June, 2022.
-          </p>
+          
+          <h1> 11th International Workshop on Vocabularies, Ontologies and Rules for the Enterprise</h1>
+          <p className="subtitle">In conjunction with the <a href="https://cbi2022.cs.vu.nl/"> 24th IEEE International Conference on Business Informatics (CBI 2022)</a>, Amsterdam, The Netherlands, 15–17 june 2022.</p>
+          
           <h2>Call for papers</h2>
+          
           <h3>About</h3>
-          <p>
-          VORTE intends to bring together researchers and practitioners in areas such as formal ontology, enterprise modeling, information systems, semantic web, model-driven engineering, business rules, and business process management. The major objective is to provide a research forum for exchanging ideas and results covering the use of ontologies and rules in various stages of the development lifecycle of enterprise systems. Fundamental research contributions include the ontological evaluation of enterprise systems and their interoperability and the investigation of the use of ontologies, rules, and AI techniques in services and business process modeling. 
-          </p>
-          <p>The goal of the VMBO workshop series is two-fold:</p>
+          <p>The 11th International Workshop on Vocabularies, Ontologies and Rules for the Enterprise (VORTE 2021) is devoted to the topics of vocabularies, ontologies and rules in the context of enterprise systems. Examples of topics covered by VORTE include the development and adaptation of foundational, business and domain ontologies for the enterprise, the use of ontologies and rules in all aspects of enterprise modeling such as business process management and services, the enhancement of rules and services with formal semantics, and the evaluation of such systems and approaches.</p>
+          <p>From the enterprise system development perspective, research topics are focused on relations of process modeling and execution languages with business ontologies and rules, and on how business ontologies and rules used in enterprise models are further propagated into technologies (e.g., linked data and semantic web) and architectures (e.g., service-oriented architectures) that enable collaboration between heterogeneous enterprise systems.</p>
+          <p>The workshop also welcomes experience reports and empirical studies that are reporting on the use of ontologies and rules in the enterprise system development life-cycle.</p>
+          
+          <h3 id="guidelines">Submission Guidelines</h3>
+          <p>We solicit two types of papers:</p>
           <ul>
-            <li>
-            to bring together researchers with an interest in value modeling and business ontology to present and discuss the current state of the art
-            </li>
-            <li>to identify key areas for further research</li>
+            <li>Short papers (up to 4 pages excluding references) discussing controversial issues in the field or describing interesting or thought-provoking ideas that are not yet fully developed; and</li>
+            <li>Full papers (up to 8 pages excluding references) describing more mature results.</li>
           </ul>
+          <p>All submissions MUST conform to the two-column format of IEEE conference proceedings (http://www2.computer.org/portal/web/cscps/formatting) and include the author's name, affiliation, and contact details.</p>
+          
+          <h3 id="guidelines">Important Dates</h3>
+          <ul>
+            <li>Abstract submission: April 10th, 2022</li>
+            <li>Paper submission: April 17th, 2022</li>
+            <li>Acceptance Notification: May 8th, 2022</li>
+            <li>Camera Ready Submission: May 15th, 2022</li>
+            <li>Workshop: June</li>
+          </ul>
+
+          <h3>Workshop Chairs</h3>
+          <ul>
+            <li>Mattia Fumagalli - Free University of Bozen-Bolzano, Italy</li>
+            mattia.fumagalli [at] unibz.it
+            <li>Pedro Paulo Favato Barcelos - Free University of Bozen-Bolzano, Italy</li>
+            pfavatobarcelos [at] unibz.it
+            <li>Giancarlo Guizzardi - Free University of Bozen-Bolzano, Italy</li>
+            giancarlo.guizzardi [at] unibz.it
+          </ul>
+          
+          <h3>Program Committee (Tentative)</h3>
+          <ul>
+            <li>Aldo Gangemi - University of Bologna, Italy</li>
+            <li>Andreas Opdahl - University of Bergen, Norway</li>
+            <li>Daniele Porello - Laboratory for Applied Ontology, ISTC-CNR, Italy</li>
+            <li>Enrico Franconi - Free University of Bozen-Bolzano, Italy</li>
+            <li>Fabian Neuhaus - University of Magdeburg, Germany</li>
+            <li>Fernanda Bai&atilde;o - PUC-Rio, Brazil</li>
+            <li>Florian Matthes - University of Munich, Germany</li>
+            <li>Frank Loebe - University of Leipzig, Germany</li>
+            <li>Frederik Gailly - Ghent University, Belgium</li>
+            <li>Gabor Bella - University of Trento, Italy</li>
+            <li>Geert Poels - Ghent University, Belgium</li>
+            <li>Giancarlo Guizzardi - Free University of Bozen-Bolzano, Italy</li>
+            <li>Guido Governatori - NICTA, Australia</li>
+            <li>Hans Weigand - Tilburg University, Netherlands</li>
+            <li>Jo&atilde;o Paulo A. Almeida - Federal University of Esp&iacute;rito Santo, Brazil</li>
+            <li>Julio Cesar Nardi, Federal Institute of Espirito Santo, Brazil</li>
+            <li>Leo Obrst, MITRE Corporation, USA</li>
+            <li>Lu&iacute;s Ferreira Pires - University of Twente, The Netherlands</li>
+            <li>Michael Gruninger - University of Toronto, Canada</li>
+            <li>Michael Lawley - Australian E-Health Research Centre, Australia</li>
+            <li>Nicholas Car - Australian National University, Australia</li>
+            <li>Nicola Guarino - Laboratory for Applied Ontology, ISTC-CNR, Italy</li>
+            <li>Oliver Kutz - Free University of Bozen-Bolzano, Italy</li>
+            <li>Oscar Pastor - Polytechnic University of Valencia, Spain</li>
+            <li>Paul Johannesson - Stockholm University, Sweden</li>
+            <li>Renata Guizzardi - University of Twente, The Netherlands</li>
+            <li>Robert Lagerstr&ouml;m - KTH Royal Institute of Technology, Sweden</li>
+            <li>Roberta Ferrario - Laboratory for Applied Ontology, ISTC-CNR, Italy</li>
+            <li>Sergio de Cesare - University of Westminster, UK</li>
+            <li>Stefano Borgo - Laboratory for Applied Ontology, Italy</li>
+            <li>Terry Roach, Capsifi, Australia</li>
+            <li>Ulrich Frank - University of Duisburg-Essen, Germany</li>
+          </ul>
+
+          <h3>Publication</h3>
+          <p>Accepted papers will be published with a dedicated IEEE proceedings volume.</p>
+
+          <h3>Contact</h3>
+          <p>All questions about submissions should be emailed to vorte2022 [at] gmail.com</p>
+
+          <h3 id="previous">Previous Editions</h3>
+
+
+
+
           <h3>Topics</h3>
           <p>Topics of interest include, but are not limited to:</p>
           <ul>
@@ -262,7 +324,6 @@ export default function Home() {
             <li>VMBO 2012, Vienna, AT</li>
             <li>VMBO 2011, Ghent, BE</li>
             <li>VMBO 2009, Amsterdam, NL</li>
-            <li>TESTE TESTE</li>
           </ul>
         </div>
       </section>
