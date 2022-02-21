2dc9c4c3c7c5a6c77d251f1ffd45d866b5838c30
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-02-21 16:36:46 +0100

    Corrections on submission link and image

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
           <p>All submissions MUST conform to the two-column format of IEEE conference proceedings (http://www2.computer.org/portal/web/cscps/formatting) and include the author's name, affiliation, and contact details.</p>
 
-          <p>Papers must be submitted as PDF files using EasyChair at https://easychair.org/conferences/?conf=vorte2022</p>
+          <p>Papers must be submitted as PDF files using EasyChair on the following submission link: <a>https://easychair.org/conferences/?conf=vorte2022"</a></p>
           
           <h3 id="dates">Important Dates</h3>
           <ul>
